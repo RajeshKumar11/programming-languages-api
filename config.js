cd0411c38a03ae935e28aa3ea06ff3ea0d56ea3f
RajeshKumar11 <kakumanurajeshkumar@gmail.com>
2024-01-18 20:53:52 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "restapitest123",
+        connectTimeout: 6000
+    },
+    listPerPage: 10,
+}
+module.exports = config;
